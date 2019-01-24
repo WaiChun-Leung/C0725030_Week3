d1400e51e57540c4f4769b48299a808b162c2e8d
WaiChunnnnn29 <c0725030@mylambton.ca>
2019-01-24 15:11:51 -0500

    Now I need to commit my update

--- C0725030_Week3/Program.cs
+++ C0725030_Week3/Program.cs
@@ -10,7 +10,25 @@ namespace C0725030_Week3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(1 + 1);
+            //Car purple = new Car(); result is "red"
+            //Vechicle dog_and_cat = new Car(); result is "red"
+            var dog_and_cat = new Car(); //result is "red"
+            var bowling = new Car();
+            Console.WriteLine("How many cars: {0}", Vechicle.howManyCars);
+        }
+    }
+    class Vechicle
+    {
+        public static int howManyCars = 0;
+        public String Color = "Blue";
+    }
+    class Car : Vechicle
+    {
+        public String Color = "Red";
+        public Car()
+        {
+            Vechicle.howManyCars++;
+            Console.WriteLine("My color is " + Color);
         }
     }
 }
