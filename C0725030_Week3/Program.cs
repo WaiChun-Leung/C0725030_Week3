a473bce09875db7fb96915e7d15fcf47a30b9d7c
WaiChunnnnn29 <c0725030@mylambton.ca>
2019-01-24 16:07:05 -0500

    Commit

--- C0725030_Week3/Program.cs
+++ C0725030_Week3/Program.cs
@@ -6,15 +6,45 @@ using System.Threading.Tasks;
 
 namespace C0725030_Week3
 {
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating();
+            if (rating == 0)
+                Console.WriteLine("Promoted to Level 1");
+            else
+                Console.WriteLine("Promoted to Level 2");
+        }
+
+        public int CalculateRating()
+        {
+            return 0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var customer = new Customer();
+            
+
             //Car purple = new Car(); result is "red"
             //Vechicle dog_and_cat = new Car(); result is "red"
-            var dog_and_cat = new Car(); //result is "red"
-            var bowling = new Car();
-            Console.WriteLine("How many cars: {0}", Vechicle.howManyCars);
+            //var dog_and_cat = new Car(); result is "red"
+            //var bowling = new Car();
+            //Console.WriteLine("How many cars: {0}", Vechicle.howManyCars);
+
+            //create a book object
+            //1.set the title
+            //2.get the title
+            //Book myBook = new Book();
+            //myBook.SetTitle ("A Separate Peace");
+            //Console.WriteLine(myBook.GetTitle());
+
         }
     }
     class Vechicle
@@ -30,5 +60,15 @@ namespace C0725030_Week3
             Vechicle.howManyCars++;
             Console.WriteLine("My color is " + Color);
         }
+    }
+    class BookCollection
+    {
+        
+    }
+    class Book : BookCollection
+    {
+        private String Title; //hide the data
+        public void SetTitle(String aTitle) { this.Title = aTitle; }
+        public String GetTitle() { return this.Title; }
     }
 }
